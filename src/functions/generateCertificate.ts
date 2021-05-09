ee6b9b6489405e504862b102b9f4090abf81b964
dariooliveirajr <dario@duotalk.io>
2021-05-08 21:41:48 -0300

    Create handlebars template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,33 @@
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import fs from "fs";
+import handlebars from "handlebars";
+import dayjs from "dayjs";
+
 import { document } from "../utils/dynamodbClient";
+import * as Handlebars from "handlebars";
 
 interface ICreateCertificate {
     id: string;
     name: string;
     grade: string;
+}
+
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    date: string;
+    medal: string;
+}
+
+const compile = async function( data: ITemplate ) {
+    const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+
+    const html = fs.readFileSync(filePath, "utf-8");
+
+    return handlebars.compile(html)(data);
+
 }
 
 export const handle = async (event) => {
@@ -17,6 +41,19 @@ export const handle = async (event) => {
             grade
         }
     }).promise();
+
+    const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+    const medal = fs.readFileSync(medalPath, "base64");
+
+    const data: ITemplate = {
+        date: dayjs().format("DD/MM/YYYY"),
+        grade,
+        name,
+        id,
+        medal,
+    }
+
+    const content = await compile(data);
 
     return {
         statusCode: 201,
